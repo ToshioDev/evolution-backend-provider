b06b082c0f7c2532c90ea957d7e747636841b054
Jonathan Marin <44934923+iTzJonathanxD@users.noreply.github.com>
2025-07-09 21:31:55 -0500

    fix: error in routes

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 
-@Controller('messages')
+@Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
@@ -28,80 +28,4 @@ export class MessageController {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
-
-  @Get()
-  async findAll() {
-    try {
-      const messages = await this.messageService.findAll();
-      return {
-        status: 'success',
-        message: 'Mensajes obtenidos exitosamente',
-        data: messages,
-        total: messages.length,
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    try {
-      const message = await this.messageService.findOne(id);
-      return {
-        status: 'success',
-        message: 'Mensaje encontrado exitosamente',
-        data: message,
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateMessageDto: any) {
-    try {
-      const message = await this.messageService.update(id, updateMessageDto);
-      return {
-        status: 'success',
-        message: 'Mensaje actualizado exitosamente',
-        data: message,
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    try {
-      await this.messageService.remove(id);
-      return {
-        status: 'success',
-        message: 'Mensaje eliminado exitosamente',
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
-  @Get('location/:locationId')
-  async findByLocation(@Param('locationId') locationId: string) {
-    try {
-      const messages = await this.messageService.findAll();
-      const filteredMessages = messages.filter(
-        (msg) => msg.locationId === locationId,
-      );
-      return {
-        status: 'success',
-        message: 'Mensajes por ubicación obtenidos exitosamente',
-        data: filteredMessages,
-        total: filteredMessages.length,
-        locationId,
-      };
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
-
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -13,38 +13,4 @@ export class MessageService {
     const created = new this.messageModel(data);
     return created.save();
   }
-
-  async findAll(): Promise<Message[]> {
-    return this.messageModel.find();
-  }
-
-  async findOne(id: string): Promise<Message> {
-    const message = await this.messageModel.findById(id);
-
-    if (!message) {
-      throw new HttpException('Mensaje no encontrado', HttpStatus.NOT_FOUND);
-    }
-
-    return message;
-  }
-
-  async update(id: string, data: Partial<Message>): Promise<Message> {
-    const message = await this.messageModel.findByIdAndUpdate(id, data, {
-      new: true,
-    });
-
-    if (!message) {
-      throw new HttpException('Mensaje no encontrado', HttpStatus.NOT_FOUND);
-    }
-
-    return message;
-  }
-
-  async remove(id: string): Promise<void> {
-    const result = await this.messageModel.findByIdAndDelete(id);
-
-    if (!result) {
-      throw new HttpException('Mensaje no encontrado', HttpStatus.NOT_FOUND);
-    }
-  }
 }
