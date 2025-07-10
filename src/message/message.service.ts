29c5d8d0eb62ddeaf73c68f1d42db27d5a39ef76
Jonathan Marin <44934923+iTzJonathanxD@users.noreply.github.com>
2025-07-09 23:49:29 -0500

    feat: .

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -2,7 +2,6 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Message, MessageDocument } from './message.schema';
-import { User } from '../user/user.schema';
 
 @Injectable()
 export class MessageService {
