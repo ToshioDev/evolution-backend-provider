8c6f426d549c3d40a793624020e6e641af56b3e4
Alexis Leon Reyes <83489240+ToshioDev@users.noreply.github.com>
2025-07-12 00:25:10 -0600

    fix error on test implement send message direct

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,11 +4,13 @@ import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 import { Message, MessageSchema } from './message.schema';
 import { UserModule } from '../user/user.module';
+import { EvolutionModule } from '../evolution/evolution.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
     UserModule,
+    EvolutionModule,
   ],
   controllers: [MessageController],
   providers: [MessageService],
